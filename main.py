dd15d395cec383dab00a21ed72ae76189e06b76a
Nirali Javiya <niralijaviya@Niralis-MacBook-Air.local>
2026-09-25 01:28:05 +0530

    url added

--- main.py
+++ main.py
@@ -111,7 +111,8 @@ app.add_middleware(
 
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://ml-frontend-pi.vercel.app"
     ],
 
     allow_credentials=True,
