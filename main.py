77783dab9d1232e005bafb1025e7bd46e72236d6
Nirali Javiya <niralijaviya@Niralis-MacBook-Air.local>
2026-09-25 09:09:43 +0530

    url added

--- main.py
+++ main.py
@@ -112,7 +112,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://ml-frontend-pi.vercel.app"
+        "https://ml-frontend-pi.vercel.app",
     ],
 
     allow_credentials=True,
@@ -121,7 +121,6 @@ app.add_middleware(
 
     allow_headers=["*"],
 )
-
 
 # ============================================================
 # 6. CHECK MODEL FILES
